f6541c47d1ed568d3874a95c8aedfaae61f360f3
Zohaib Saeed <zohaib.saeed1259@gmail.com>
2024-10-19 14:20:32 +0000

    about added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const About = () => {
+  return <>About Us</>;
+};
+export default About;
--- src/components/FlightSeatSelector.tsx
+++ src/components/FlightSeatSelector.tsx
@@ -1,46 +1,57 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import SeatButton from './SeatButton';
-import { IconSteeringWheel } from '@tabler/icons-react';
+"use client";
+import React, { useEffect, useState } from "react";
+import SeatButton from "./SeatButton";
+import { IconSteeringWheel } from "@tabler/icons-react";
 
 interface Seat {
-    readonly seatNumber: number;
-    reservedBy: string | null;
-    readonly price: number
-    readonly category: string;
+  readonly seatNumber: number;
+  reservedBy: string | null;
+  readonly price: number;
+  readonly category: string;
 }
-
 
 interface SeatSelectorProps {
-    setSelectedSeats: (open: number) => void;
-    selectedSeats: number[]
-    seats: Seat[]
-    setSeats: (seats: Seat[]) => void;
-    handleSeatSelect: (seatsIndex: number, seatType: string) => void;
-    steering?: boolean | true
+  setSelectedSeats: (open: number) => void;
+  selectedSeats: number[];
+  seats: Seat[];
+  setSeats: (seats: Seat[]) => void;
+  handleSeatSelect: (seatsIndex: number, seatType: string) => void;
+  steering?: boolean | true;
 }
-const FlightSeatSelector: React.FC<SeatSelectorProps> = ({ steering, seats, setSeats, selectedSeats, handleSeatSelect, setSelectedSeats }) => {
+const FlightSeatSelector: React.FC<SeatSelectorProps> = ({
+  steering,
+  seats,
+  setSeats,
+  selectedSeats,
+  handleSeatSelect,
+  setSelectedSeats,
+}) => {
+  const totalSeats = seats.length;
 
-    const totalSeats = seats.length;
+  const renderItems = seats.map((seat, index) => (
+    <React.Fragment key={seat.seatNumber}>
+      <SeatButton
+        handleSeatSelect={handleSeatSelect}
+        seatNumber={seat.seatNumber}
+        isReserved={seat.reservedBy}
+      />
+      {/* Insert empty div after every second seat, but not after the last group of seats */}
+      {index % 2 === 1 && index < seats.length - 1 && (
+        <div className="empty-div" />
+      )}
+    </React.Fragment>
+  ));
 
-    const renderItems = seats.map((seat, index) => (
-        <React.Fragment key={seat.seatNumber}>
-            <SeatButton handleSeatSelect={handleSeatSelect} seatNumber={seat.seatNumber} isReserved={seat.reservedBy} />
-            {/* Insert empty div after every second seat, but not for the last five elements */}
-            {((index + 1) % 6 === 2)  && <div className="empty-div" />}
-        </React.Fragment>
-    ));
-
-    return (
-        <div className='flex justify-center'>
-            <div className='flex justify-center flex-col items-end'>
-                {steering && <IconSteeringWheel size={50} className='text-muted-foreground' />}
-                <div className='grid grid-cols-7 gap-2'>
-                    {renderItems}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-center">
+      <div className="flex justify-center flex-col items-end">
+        {steering && (
+          <IconSteeringWheel size={50} className="text-muted-foreground" />
+        )}
+        <div className="grid grid-cols-9 gap-2">{renderItems}</div>
+      </div>
+    </div>
+  );
 };
 
 export default FlightSeatSelector;
